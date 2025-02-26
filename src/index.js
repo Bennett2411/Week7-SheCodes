c391e15b74584994dd97d22b7bbaede52fa25736
Bennett2411 <sharna.bennett@hotmail.com>
2025-02-26 08:48:39 +0000

    added search functionality with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshweather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "e2f90841bba6oe00351f9aa2e3877b4t";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  axios.get(apiURL).then(refreshweather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#searchform");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
