be17b6a3758c1be803f898a3e8aaa28b0b0870a1
Bennett2411 <sharna.bennett@hotmail.com>
2025-02-26 09:08:33 +0000

    updated axios link

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function refreshweather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
@@ -9,7 +10,7 @@ function refreshweather(response) {
 function searchCity(city) {
   let apiKey = "e2f90841bba6oe00351f9aa2e3877b4t";
   let apiURL =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=e2f90841bba6oe00351f9aa2e3877b4t&units=metric";
   axios.get(apiURL).then(refreshweather);
 }
 
